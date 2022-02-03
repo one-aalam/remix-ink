7c671b4d98a21d98f2206f86506b5077b8cf2377
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2022-02-03 10:34:42 +0530

    1.d - Styling - The Remix way

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { LiveReload, Outlet } from "remix";
+import { LiveReload, Outlet, LinksFunction, Links } from "remix";
 
 import Header from '~/components/Header'
 import Nav from '~/components/Nav'
@@ -9,6 +9,14 @@ import blogLayoutStyleUrl from '~/styles/layout-blog.css'
 import blogPageStyleUrl from '~/styles/page-blog-index.css'
 import blogPostStyleUrl from '~/styles/page-blog-post.css'
 
+export let links: LinksFunction = () => {
+    return [
+        { rel: 'stylesheet', href: appStyleUrl },
+        { rel: 'stylesheet', href: blogLayoutStyleUrl },
+        { rel: 'stylesheet', href: blogPageStyleUrl },
+        { rel: 'stylesheet', href: blogPostStyleUrl }
+    ]
+}
 
 export default function App() {
   return (
@@ -16,10 +24,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <title>Remix: Engineer modern full-stack apps, joyfully! </title>
-        <link rel="stylesheet" href={appStyleUrl} />
-        <link rel="stylesheet" href={blogLayoutStyleUrl} />
-        <link rel="stylesheet" href={blogPageStyleUrl} />
-        <link rel="stylesheet" href={blogPostStyleUrl} />
+        <Links/>
       </head>
       <body>
         <div className="container">
